3d7d8ac6f77912f29f3876f2acc736c11caa3609
Daniil-Pest <102615520+Daniil-Pest@users.noreply.github.com>
2026-05-23 02:18:13 +0700

    Create TokenType.java

---
+++ app/src/main/java/tsu/mk/lexis/TokenType.java
@@ -0,0 +1,30 @@
+package tsu.mk.lexis;
+
+/* {@code INT_LITERAL} represents 'k'; {@code IDENTIFIER} represents 'a' in the parsing table */
+public enum TokenType {
+	LEFT_PAREN,
+	RIGHT_PAREN,
+	LEFT_BRACKET,
+	RIGHT_BRACKET,
+	LEFT_BRACE,
+	RIGHT_BRACE,
+	COMMA,
+	MINUS,
+	PLUS,
+	SEMICOLON,
+	SLASH,
+	STAR,
+	EQUAL,
+	IDENTIFIER,
+	WHILE,
+	IF,
+	ELSE,
+	INPUT,
+	OUTPUT,
+	ARRAY,
+	INT_TYPE,
+	INT_LITERAL,
+	GREATER,
+	LESS,
+	EOF;
+}
